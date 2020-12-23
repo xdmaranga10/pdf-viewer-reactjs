65d64bc53ee7e88736ac301b385979836205394c
xdmaranga10 <xdmaranga4@gmail.com>
2020-12-23 18:46:08 +0800

    modal update 10

--- src/components/RenderPdf.js
+++ src/components/RenderPdf.js
@@ -252,7 +252,7 @@ const RenderPdf = ({
             )
 
             setThumbnails(thumbnailList)
-            //setParentThumbnails(thumbnailList)
+            setParentThumbnails(thumbnailList)
         }
     }
 
